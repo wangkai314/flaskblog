644110911511f6edd3647f8ee590c93307954bfd
wangkai314 <53928665+wangkai314@users.noreply.github.com>
2020-02-23 18:34:20 -0500

    Add files via upload

---
+++ Flask_Blog/flaskblog/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+
+app = Flask(__name__)
+app.config['SECRET_KEY']='be82c8f12c95db13e54473f344bc9301'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'wkai05108@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Buzhidao123+'
+mail = Mail(app)
+
+from flaskblog import routes
